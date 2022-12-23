42a8cfd825a87dbdf7a84e0bd8303c1a2314ed84
AndersonChaves <andersonchaves.net@gmail.com>
2022-12-23 16:01:25 -0300

    Code for analyzing static clustering, dynamic silhouette

--- exp1.py
+++ exp1.py
@@ -14,7 +14,32 @@ global_configurations_path = "experiment-metadata/djensemble-exp1.config"
 config_manager = ConfigManager(global_configurations_path)
 
 cur_time = str(datetime.now())
-results_directory = "results/cfsr-static_clustering_dyn_silhouette/" + cur_time + "/"
+results_directory = "results/cfsr-static_clustering_dyn_silhouette-10:13/" + cur_time + "/"
+
+def calculate_silhouette_through_time(initial_instant, window_size,
+                                        file_name, clustering_type = "dynamic",
+                                      clustering = None):
+    if clustering_type == "static" and clustering is None:
+        raise Exception("Clustering is static, but empty")
+    t_instant = initial_instant
+    while (t_instant < ((365 * 4) + window_size)):
+        with open(file_name, "a") as f:
+            start = time()
+            window_series = ds_manager.read_window(t_instant, t_instant + window_size)
+            if window_series.shape[0] < window_size:
+                break
+            gld_list = ct.calculate_gld_list_from_dataset(window_series)
+            ct.normalize_gld_list(gld_list)
+            local_dynamic_silhouette = silhouette_score(gld_list, clustering)
+
+            f.write("Frame " + str(t_instant) + ": \n")
+            f.write("----Clustering: " + str(clustering) + ": \n")
+            f.write(" ".join(str(item) for item in clustering))
+            f.write("----Local Dynamic Silhouette Score: " + str(local_dynamic_silhouette) + "\n")
+            local_static_clustering_time = str(time() - start)
+            f.write("----Silhouette Time: " + str(local_static_clustering_time) + "\n")
+            t_instant += window_size
+
 
 if __name__ == '__main__':
     create_directory_if_not_exists(results_directory)
@@ -22,7 +47,7 @@ if __name__ == '__main__':
     # Load the Dataset
     ds_manager = DatasetManager(config_manager.get_config_value("dataset_path"))
     ds_manager.loadDataset(ds_attribute=config_manager.get_config_value("target_attribute"))
-    #ds_manager.filter_by_region((11, 16), (11, 16))
+    ds_manager.filter_by_region((10, 13), (10, 13))
 
     # ******************** FIRST TEST: PERFORM GLOBAL CLUSTERING **************
     start = time()
@@ -40,7 +65,6 @@ if __name__ == '__main__':
     np.save(results_directory + "gld_list-full", gld_list)
     np.save(results_directory + "clustering-full", global_clustering)
 
-
     file_name = results_directory + "results" + cur_time + ".txt"
     with open(file_name, "w") as f:
         f.write("----Number of Groups: " + str(global_number_of_groups) + "\n")
@@ -49,21 +73,26 @@ if __name__ == '__main__':
         f.write("----Global Silhouette Score: " + str(global_silhouette) + "\n")
         f.write("----Global Clustering Time: " + str(global_clustering_time) + "\n")
 
+    with open(file_name, "a") as f:
+        f.write("*************Calculating Silhouette Trough Time: Global Clustering" "\n")
+    calculate_silhouette_through_time(0, window_size=28, file_name=file_name,
+                                      clustering_type="static", clustering=global_clustering)
+
     # ******************** SECOND TEST: PERFORM LOCAL CLUSTERING, ANALYZE SILHOUETE Along windows **************
     start = time()
-    gld_list, local_static_clustering, local_dynamic_silhouette = ct.cluster_dataset(ds_manager.read_window(0, 96))
-    local_static_number_of_groups = len(set(local_static_clustering))
+    gld_list, clustering, local_dynamic_silhouette = ct.cluster_dataset(ds_manager.read_window(0, 96))
+    local_static_number_of_groups = len(set(clustering))
     local_static_clustering_time = str(time() - start)
-    print("----Local Static Clustering: ", local_static_clustering)
+    print("----Local Static Clustering: ", clustering)
     print("----Local Dynamic Silhouette Score: ", local_dynamic_silhouette)
     print("----Local Static Clustering Time: " + local_static_clustering_time)
 
     file_name = results_directory + "results" + cur_time + ".txt"
-    with open(file_name, "w") as f:
+    with open(file_name, "a") as f:
         f.write("----Frame 0: ""\n")
         f.write("----Number of Groups: " + str(local_static_number_of_groups) + "\n")
-        f.write("----Local static Clustering: " + str(local_static_clustering) + "\n")
-        f.write(" ".join(str(item) for item in local_static_clustering))
+        f.write("----Local static Clustering: " + str(clustering) + "\n")
+        f.write(" ".join(str(item) for item in clustering))
         f.write("----Local Initial Dynamic Silhouette Score: " + str(local_dynamic_silhouette) + "\n")
         f.write("----Local Static Clustering Time: " + str(local_static_clustering_time) + "\n")
 
@@ -72,14 +101,16 @@ if __name__ == '__main__':
         with open(file_name, "a") as f:
             start = time()
             window_series = ds_manager.read_window(t_instant, t_instant+28)
+            if window_series.shape[0] < 28:
+                break
             gld_list = ct.calculate_gld_list_from_dataset(window_series)
             ct.normalize_gld_list(gld_list)
-            local_dynamic_silhouette = silhouette_score(gld_list, local_static_clustering)
+            local_dynamic_silhouette = silhouette_score(gld_list, clustering)
 
             f.write("----STATIC CLUSTERING, DYNAMIC SILHOUETTE Frame " + str(t_instant) + ": \n")
             f.write("----Number of Groups: " + str(local_static_number_of_groups) + "\n")
-            f.write("----Clustering: " + str(local_static_clustering) + "\n")
-            f.write(" ".join(str(item) for item in local_static_clustering))
+            f.write("----Clustering: " + str(clustering) + "\n")
+            f.write(" ".join(str(item) for item in clustering))
             f.write("----Local Dynamic Silhouette Score: " + str(local_dynamic_silhouette) + "\n")
             local_static_clustering_time = str(time() - start)
             f.write("----Silhouette Time: " + str(local_static_clustering_time) + "\n")
@@ -94,15 +125,15 @@ if __name__ == '__main__':
         with open(file_name, "a") as f:
             start = time()
             frame_window_series = ds_manager.read_window(t_instant, t_instant+28)
-            if frame_window_series is not None:
-                gld_list, local_clustering = local_cls_manager.update_clustering(frame_window_series)
-            else:
+            if frame_window_series.shape[0] < 28:
                 break
-            np.save(results_directory + "gld_list-t=" + str(t_instant), gld_list)
-            np.save(results_directory + "clustering-t=" + str(t_instant), local_clustering)
-
+            gld_list, local_clustering = local_cls_manager.update_clustering(frame_window_series)
             print("----Local Clustering: ", local_clustering)
-            print("----Local Silhouette Score: ", silhouette_score(gld_list, local_clustering))
+            if len(set(local_clustering)) == 1:
+                print("----Local Silhouette Score: ", "N/A\n")
+            else:
+                print("----Local Silhouette Score: ", silhouette_score(gld_list, local_clustering))
+            f.write("----Test 3: PERFORM DYNAMIC CLUSTERING, ANALYZE SILHOUETTE Along windows\n")
             f.write("----Local Clustering t = " + str(t_instant) + " to " + str(t_instant + 28) + ": " + "\n")
             f.write(" ".join(str(item) for item in local_clustering) + "\n")
             if len(set(local_clustering)) == 1:
@@ -116,8 +147,9 @@ if __name__ == '__main__':
 
             x = history_gld_list[:, 0]
             y = history_gld_list[:, 2]
-            view.save_figure_as_scatter_plot(x, y, history_clustering, results_directory + "clustering-" + str(t_instant), annotate=False)
-            view.save_figure_from_matrix(np.reshape(local_clustering, frame_window_series.shape[1:]), results_directory + "matrix-" + str(t_instant))
+            #view.save_figure_as_scatter_plot(x, y, history_clustering, results_directory + "clustering-" + str(t_instant), annotate=False)
+            if t_instant % (28 * 4) == 0:
+              view.save_figure_from_matrix(np.reshape(local_clustering, frame_window_series.shape[1:]), results_directory + "matrix-" + str(t_instant))
             t_instant += 28
 
 
