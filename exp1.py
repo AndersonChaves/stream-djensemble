a0ad432011a0a0c691ebebfe6dd569831eca629c
AndersonChaves <andersonchaves.net@gmail.com>
2022-12-27 17:25:24 -0300

    Code for adding embedding method: parcorr

--- exp1.py
+++ exp1.py
@@ -14,7 +14,9 @@ global_configurations_path = "experiment-metadata/djensemble-exp1.config"
 config_manager = ConfigManager(global_configurations_path)
 
 cur_time = str(datetime.now())
-results_directory = "results/cfsr-static_clustering_dyn_silhouette-10:13/" + cur_time + "/"
+results_directory = "results/cfsr-static_clustering_dyn_silhouette-10:14/" + cur_time + "/"
+
+embedding_method="parcorr"
 
 def calculate_silhouette_through_time(initial_instant, window_size,
                                         file_name, clustering_type = "dynamic",
@@ -30,8 +32,8 @@ def calculate_silhouette_through_time(initial_instant, window_size,
             window_series = ds_manager.read_window(t_instant, t_instant + window_size)
             if window_series.shape[0] < window_size:
                 break
-            gld_list = ct.calculate_gld_list_from_dataset(window_series)
-            ct.normalize_gld_list(gld_list)
+            gld_list = ct.get_embedded_series_representation(window_series, method="parcorr")
+            ct.normalize_embedding_list(gld_list)
             history_gld_list.append(gld_list)
             local_dynamic_silhouette = silhouette_score(gld_list, clustering)
 
@@ -50,15 +52,14 @@ if __name__ == '__main__':
     # Load the Dataset
     ds_manager = DatasetManager(config_manager.get_config_value("dataset_path"))
     ds_manager.loadDataset(ds_attribute=config_manager.get_config_value("target_attribute"))
-    ds_manager.filter_by_region((10, 13), (10, 13))
+    ds_manager.filter_by_region((10, 14), (10, 14))
 
     # ******************** FIRST TEST: PERFORM GLOBAL CLUSTERING **************
     start = time()
-    gld_list, global_clustering, global_silhouette = ct.cluster_dataset(ds_manager.read_all_data())
+    gld_list, global_clustering, global_silhouette = ct.cluster_dataset(ds_manager.read_all_data(),
+                                                                        embedding_method=embedding_method)
     global_number_of_groups = len(set(global_clustering))
     global_clustering_time = str(time() - start)
-
-    local_cls_manager = ClusterManager(config_manager, n_clusters=global_number_of_groups)
 
     file_name = results_directory + "results" + cur_time + ".txt"
     with open(file_name, "w") as f:
@@ -77,7 +78,8 @@ if __name__ == '__main__':
     # ******************** SECOND TEST: PERFORM LOCAL CLUSTERING, ANALYZE SILHOUETE Along windows **************
     silhouete_name = "Silh TEST2"
     start = time()
-    gld_list, clustering, local_dynamic_silhouette = ct.cluster_dataset(ds_manager.read_window(0, 28*4))
+    gld_list, clustering, local_dynamic_silhouette = ct.cluster_dataset(ds_manager.read_window(0, 28*4),
+                                                                        embedding_method=embedding_method)
     local_static_number_of_groups = len(set(clustering))
     local_static_clustering_time = str(time() - start)
 
@@ -116,7 +118,7 @@ if __name__ == '__main__':
     # Perform local dynamic Clustering
     silhouete_name = "Silh TEST3"
     local_clustering = []
-    history_gld_list = np.empty((0, 4))
+    history_gld_list = np.empty((0, 5))
     t_instant = 0
     it_gld_list = iter(history_gld_list_previous)
     while(t_instant < ((365*4)+28)):
@@ -126,6 +128,7 @@ if __name__ == '__main__':
             if frame_window_series.shape[0] < 28:
                 break
             gld_list = next(it_gld_list)
+            local_cls_manager = ClusterManager(config_manager, n_clusters=local_static_number_of_groups)
             gld_list, local_clustering = local_cls_manager.update_clustering(frame_window_series, gld_list)
             f.write("----Test 3: PERFORM DYNAMIC CLUSTERING, ANALYZE SILHOUETTE Along windows\n")
             f.write("----Local Clustering t = " + str(t_instant) + " to " + str(t_instant + 28) + ": " + "\n")
@@ -142,6 +145,7 @@ if __name__ == '__main__':
             x = history_gld_list[:, 0]
             y = history_gld_list[:, 2]
             #view.save_figure_as_scatter_plot(x, y, history_clustering, results_directory + "clustering-" + str(t_instant), annotate=False)
+            print("Generating Figure")
             if t_instant % (28 * 4) == 0:
               view.save_figure_from_matrix(np.reshape(local_clustering, frame_window_series.shape[1:]), results_directory + "matrix-" + str(t_instant))
             t_instant += 28
